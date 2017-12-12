2c88657cfb931e1f2d39fa45b2543448a4db44dc
ThijsdR <tdrooij@gmail.com>
2017-12-12 09:13:28 +0100

    Init project

---
+++ src/main/java/Inf0_B0t.java
@@ -0,0 +1,19 @@
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Inf0_B0t extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        // ToDo
+    }
+
+    public String getBotUsername() {
+        // ToDo
+        return null;
+    }
+
+    public String getBotToken() {
+        // ToDo
+        return null;
+    }
+}
